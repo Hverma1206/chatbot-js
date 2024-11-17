d77922435fd153aee5dd2d21ba648bf1d2bd1747
Himanshu Verma <145604241+Hverma1206@users.noreply.github.com>
2024-11-17 22:29:33 +0530

    changes

--- public/script.js
+++ public/script.js
@@ -4,12 +4,15 @@ const chat = document.getElementById('chat');
 const sendBtn = document.getElementById('send-btn');
 
 function sendMessage() {
+    
     const message = input.value.trim();
     if (message) {
         addMessage(`You: ${message} `, 'user');
         socket.emit('user message', message);
-        input.value = '';
-        input.focus();  
+        input.value = ''
+
+        input.focus() 
+
     }
 }
 
@@ -17,12 +20,14 @@ function addMessage(text, sender) {
     const messageElement = document.createElement('div');
     messageElement.classList.add('message', sender);
     messageElement.textContent = text;
-    chat.appendChild(messageElement);
+
+    chat.appendChild(messageElement)
     chat.scrollTop = chat.scrollHeight;  
 }
 
 input.addEventListener('keypress', (event) => {
     if (event.key === 'Enter') {
+
         sendMessage();
     }
 });
@@ -30,5 +35,7 @@ input.addEventListener('keypress', (event) => {
 sendBtn.addEventListener('click', sendMessage);
 
 socket.on('bot response', (response) => {
-    addMessage(`Bot: ${response} 🤖`, 'bot');
+
+    addMessage(`Bot: ${response} 🤖`, 'bot')
+
 });
